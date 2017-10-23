d7cc9ac52eb94ccda91d573764804fbb253855e8
Kayla Danielle Adams <kayladanielleadams@Kaylas-MacBook-Pro.local>
2017-10-23 12:12:57 -0500

    minor updates to the database

--- server/config/db.ts
+++ server/config/db.ts
@@ -3,7 +3,7 @@ import * as mysql from 'mysql';
  export let pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DATABASE_URL,
-    user: process.env.DATABASE_USER,
+    user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME
 });
