6a33da4b7f7aae3e96a02eab38c14a276983cbf6
Kayla Danielle Adams <kayladanielleadams@Kaylas-MacBook-Pro.local>
2017-10-23 11:54:32 -0500

    Modified db.ts to use environment variables for Heroku

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,10 +2,10 @@ import * as mysql from 'mysql';
 
  export let pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'kdadams',
-    password: 'kayla',
-    database: 'AngularBlog'
+    host: process.env.DATABASE_URL,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 
